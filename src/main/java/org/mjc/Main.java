e113975c47340e423a1caec2d7726a8457e2982b
erikbayerlein <erik.bayer@icloud.com>
2025-06-27 10:45:44 -0300

    feat: update main method to integrate MjcCompiler and AntlrParser for compilation

--- src/main/java/org/mjc/Main.java
+++ src/main/java/org/mjc/Main.java
@@ -1,17 +1,22 @@
 package org.mjc;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.mjc.mjc.MjcCompiler;
+import org.mjc.parser.AntlrParser;
+import org.mjc.visitor.types.TypeCheckingVisitor;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream inputStream = new FileInputStream("src/test/resources/testFiles/Factorial.java");
+
+        MjcCompiler compiler = MjcCompiler.builder()
+                .parser(new AntlrParser())
+                .semanticAnalysis(new TypeCheckingVisitor())
+                .build();
+
+        compiler.compile(inputStream, System.out);
     }
 }
